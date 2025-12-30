f54b3d659931ccf3e724f4b8e760cb7b6053db4f
maximedoaw <maximedoaw204@gmail.com>
2025-12-30 21:20:47 +0100

    Synchronisation avec inngest

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,13 +8,19 @@
  * @module
  */
 
+import type * as http from "../http.js";
+import type * as projects from "../projects.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
 
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  http: typeof http;
+  projects: typeof projects;
+}>;
 
 /**
  * A utility for referencing Convex functions in your app's public API.
